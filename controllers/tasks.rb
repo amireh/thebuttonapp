ef0ca2f4015403fe0dd152814cda957a2b5bc587
Ahmad Amireh <ahmad.amireh@gmail.com>
2013-01-15 16:02:07 +0300

    tasks can now be edited, notes can be deleted

--- controllers/tasks.rb
+++ controllers/tasks.rb
@@ -6,6 +6,14 @@ route_namespace '/tasks' do
     erb :"/tasks/new"
   end
 
+  get '/:id/edit' do |task_id|
+    unless @t = current_user.tasks.get(task_id.to_i)
+      halt 400, "No such task"
+    end
+
+    erb :"/tasks/new"
+  end
+
   get '/history' do
     erb :"/tasks/history"
   end
@@ -63,21 +71,69 @@ route_namespace '/tasks' do
     redirect '/'
   end
 
-  post do
-    e = @user.employers.get(params[:employer_id].to_i)
+  delete '/:id/notes/:note_id' do |task_id, note_id|
+    unless t = current_user.tasks.get(task_id.to_i)
+      halt 400, "No such task"
+    end
 
-    cws = @user.current_session
+    unless n = t.notes.get(note_id.to_i)
+      halt 400, 'No such note'
+    end
 
-    t = @user.tasks.create({ name: params[:name], employer: e })
-    if t.saved?
-      params[:notes] && params[:notes].each { |n| t.notes.create({ content: n })}
+    if n.destroy
+      flash[:notice] = "Note removed!"
+    else
+      flash[:error] = n.all_errors
+    end
 
-      if cws
-        cws.task = t
-        cws.save
+    redirect '/'
+  end
+
+  def extract_tags_and_details(params)
+    name = params[:task][:name]
+    details = params[:task][:details]
+
+    tags = []
+    if name =~ /#\w+/
+      tag_names = []
+      name.gsub(/#(\w|\-|\_)+\s*/) { |match|
+        tag_names << match.strip.gsub('#', '')
+      }
+
+      if details
+        details.gsub(/#(\w|\-|\_)+\s*/) { |match|
+          tag_names << match.strip.gsub('#', '')
+        }
       end
 
-      flash[:notice] = "Task #{t.name} has been registered."
+      tag_names.each do |name|
+        tags << @user.tags.first_or_create({ name: name })
+      end
+    end
+
+    # any detailed description written? (has to push two \n\n after the task name)
+    unless details
+      parts = name.split("\n")
+      details = ''
+      if parts.length > 2
+        # it has a detailed description
+        name    = parts[0]
+        details = parts[2..-1].join("\n")
+      end
+    end
+
+    [name, tags, details]
+  end
+
+  post '/:id' do |task_id|
+    unless t = current_user.tasks.get(task_id.to_i)
+      halt 400, "No such task"
+    end
+
+    name, tags, details = extract_tags_and_details(params)
+
+    if t.update({ name: name, tags: tags, details: details })
+      flash[:notice] = "Task #{t.name} has been updated."
     else
       flash[:error] = t.all_errors
     end
